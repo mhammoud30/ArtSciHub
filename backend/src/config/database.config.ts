d57cf4f9c515646ba98665ddd87db4d2c232a309
Mohamad Hammoud <78760617+mhammoud30@users.noreply.github.com>
2024-12-02 17:53:00 +0300

    changes to get ready for deploying based on recommendations

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -1,12 +1,34 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('database', () => ({
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  name: process.env.DATABASE_NAME,
-  synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
-  autoLoadEntities:
-    process.env.DATABASE_LOAD_ENTITIES === 'true' ? true : false,
-}));
+export default registerAs('database', () => {
+  const dbUrl = process.env.DATABASE_URL;
+
+  let config = {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+    synchronize: process.env.DATABASE_SYNC === 'true',
+    autoLoadEntities: process.env.DATABASE_LOAD_ENTITIES === 'true',
+  };
+
+  if (dbUrl) {
+    try {
+      const url = new URL(dbUrl);
+      config = {
+        host: url.hostname,
+        port: parseInt(url.port, 10) || 5432,
+        username: url.username,
+        password: url.password,
+        name: url.pathname.slice(1), // Remove the leading '/'
+        synchronize: process.env.DATABASE_SYNC === 'true',
+        autoLoadEntities: process.env.DATABASE_LOAD_ENTITIES === 'true',
+      };
+    } catch (error) {
+      console.error('Invalid DATABASE_URL format:', error);
+    }
+  }
+
+  return config;
+});
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'frontend/dist/frontend'));
+  app.setViewEngine('html');
   /**
    * Use validation pipes globally
    */
